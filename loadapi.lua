5ca471102c167c469387be67d8fcd88a2166ed8a
Nova <hazoldevz@gmail.com>
2025-03-15 13:42:13 -0400

    Update loadapi.lua

--- loadapi.lua
+++ loadapi.lua
@@ -1,5 +1,8 @@
 local StarterGui = game:GetService("StarterGui")
 
+-- Wait until SetCore is available
+repeat wait() until pcall(function() return StarterGui:SetCore end)
+
 StarterGui:SetCore("SendNotification", {
     Title = "API Injected",
     Text = "Your script has been successfully injected!",
